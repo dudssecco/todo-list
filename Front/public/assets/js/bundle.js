7d99bfbbff5c995674b631ce33df7b38eeff4d44
Eduardo Secco <dudssecco.code@gmail.com>
2024-05-02 22:26:19 -0300

    Create Base Front

---
+++ Front/public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./src/scripts/index.js ***!
+  \******************************/
+console.log('teste');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
